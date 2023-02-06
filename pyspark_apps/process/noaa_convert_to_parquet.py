a085953dcebf12ef8dd9a346d9cc5bb12c2bbdc0
Jasmine Pate <jasminepate57@gmail.com>
2023-02-06 14:01:45 -0500

    convert to parquet

--- pyspark_apps/process/noaa_convert_to_parquet.py
+++ pyspark_apps/process/noaa_convert_to_parquet.py
@@ -2,8 +2,8 @@ import os
 
 import boto3
 from pyspark.sql import SparkSession
-from utils.helper_functions import *
 
+from utils.helper_functions import *
 
 s3_client = boto3.client('s3')
 
@@ -12,8 +12,7 @@ def main():
     dir_path = os.path.dirname(os.path.dirname(os.path.realpath(__file__)))
     
     spark = SparkSession.builder.appName("noaa-convert-to-parquet").getOrCreate()
-    for file in s3_client.list_objects(Bucket=params['bronze_bucket'], Delimiter='/'):
-        convert_to_parquet(spark, dir_path + file, params)
+    convert_to_parquet(spark, dir_path + f"noaa.{format}", params)
 
 def convert_to_parquet(spark, file_path, format, params):
     noaa_df = spark.read \
@@ -21,11 +20,11 @@ def convert_to_parquet(spark, file_path, format, params):
         .option("header", "true") \
         .option("delimiter", ",") \
         .option("inferSchema", "true") \
-        .load(f"s3a://{params['bronze_bucket']}/{file_path}.{format}")
+        .load(f"s3://{params['bronze_bucket']}/{file_path}.{format}")
 
     noaa_df.write \
         .format("parquet") \
-        .save(f"s3a://{params['silver_bucket']}/{file_path}/", mode="overwrite")
+        .save(f"s3://{params['silver_bucket']}/{file_path}/", mode="overwrite")
 
 if __name__ == "__main__":
     main()
--- pyspark_apps/process/opensky_convert_to_parquet.py
+++ pyspark_apps/process/opensky_convert_to_parquet.py
@@ -12,8 +12,7 @@ def main():
     dir_path = os.path.dirname(os.path.dirname(os.path.realpath(__file__)))
     
     spark = SparkSession.builder.appName("opensky-convert-to-parquet").getOrCreate()
-    for file in s3_client.list_objects(Bucket=params['bronze_bucket'], Delimiter='/'):
-        convert_to_parquet(spark, dir_path + file, params)
+    convert_to_parquet(spark, dir_path + f"opensky.{format}", params)
 
 def convert_to_parquet(spark, file_path, format, params):
     sky_df = spark.read \
@@ -21,11 +20,11 @@ def convert_to_parquet(spark, file_path, format, params):
         .option("header", "true") \
         .option("delimiter", ",") \
         .option("inferSchema", "true") \
-        .load(f"s3a://{params['bronze_bucket']}/{file_path}.{format}")
+        .load(f"s3://{params['bronze_bucket']}/{file_path}.{format}")
 
     sky_df.write \
         .format("parquet") \
-        .save(f"s3a://{params['silver_bucket']}/{file_path}/", mode="overwrite")
+        .save(f"s3://{params['silver_bucket']}/{file_path}/", mode="overwrite")
 
 if __name__ == "__main__":
     main()
