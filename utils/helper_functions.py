dd33fb634460dcc78e8f647e223ec4b6b5a0d7e6
Jasmine Pate <jasminepate57@gmail.com>
2023-02-06 14:00:09 -0500

    dynamic parameter retrieval

--- utils/helper_functions.py
+++ utils/helper_functions.py
@@ -1,31 +1,36 @@
 import argparse
 import boto3
 import logging
+from botocore.exceptions import ClientError
 
 ssm_client = boto3.client('ssm')
 sts_client = boto3.client('sts')
 
-logger = logging.basicConfig(format='[%(asctime)s] %(levelname)s - %(message)s', level=logging.INFO)
+logging.basicConfig(format='[%(asctime)s] %(levelname)s - %(message)s', level=logging.INFO)
 
 NOAA_RESPONSE_TYPES = ['forecast', 'forecastHourly', 'forecastGridData']
 
-def get_parameters():
-    params = {}
-    params = {
-        'opensky_username': ssm_client.get_parameter(Name='/opensky/username')['Parameter']['Value'],
-        'opensky_password': ssm_client.get_parameter(Name='/opensky/password')['Parameter']['Value'],
-        'bronze_bucket': ssm_client.get_parameter(Name='/aw-metar/bronze_bucket')['Parameter']['Value'],
-        'silver_bucket': ssm_client.get_parameter(Name='/aw-metar/silver_bucket')['Parameter']['Value'],
-        'work_bucket': ssm_client.get_parameter(Name='/aw-metar/work_bucket')['Parameter']['Value'],
-    }    
-    return params
+def get_parameters(requested_params) -> list:
+    try:
+        params = {
+            'opensky_username': '/opensky/username',
+            'opensky_password': '/opensky/password',
+            'bronze_bucket': '/aw-metar/bronze_bucket',
+            'silver_bucket': '/aw-metar/silver_bucket',
+            'work_bucket': '/aw-metar/work_bucket'
+        }
+        return {x: ssm_client.get_parameter(Name=params[x])['Parameter']['Value'] for x in requested_params} 
+    except ClientError as e:
+        logging.error(e)   
 
 def parse_args():
     """Parse argument values from command-line"""
 
     parser = argparse.ArgumentParser(description='Arguments required for script.')
     parser.add_argument('-b', '--bucket', required=False, choices=['work', 'bronze', 'prod'], help='Environment')
-    parser.add_argument('-a', '--app_name', required=True, help='Environment')
+    parser.add_argument('-a', '--app_name', required=False, help='Environment')
+    parser.add_argument('-t', '--job-type', required=False, choices=['process', 'analyze'], help='process or analysis')
+    parser.add_argument('-c', '--crawler-name', required=False, help='Name of EC2 Keypair')
 
     args = parser.parse_args()
     return args
